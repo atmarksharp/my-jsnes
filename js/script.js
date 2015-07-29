a75d1c44c4094a20541ff1b1fbe867d1fc577803
atmarksharp <atmarksharp@live.jp>
2015-07-29 18:37:50 +0900

    Add rom

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ var nes;
 $(function() {
   nes = new JSNES({
     'ui': $('#emulator').JSNESUI({
-        "Roms": [
-            ['Super Mario Bros.', 'roms/super-mario-bros-j.nes']
+        "NES": [
+            ['Super Mario Bros.', 'roms/super-mario-bros-j.nes'],
+            ['Rockman 2', 'roms/rockman-2-j.nes']
         ]
     })
   });
